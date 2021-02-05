f4cc368ef646536d9bd162cbb6e512559781f434
Charles Robinson <charlesdalerobinson@gmail.com>
2021-02-05 13:58:51 -0800

    Added html routes to application.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,24 @@
-// Enter routes here to access index.html and stats.html
+// Enter routes here to access index.html, exercise.html, and stats.html
+
+const router = require("express").Router();
+const path = require("path");
+
+// Home page
+router.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Add exercise page
+router.get("/exercise", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// Workout stats page
+router.get("/stats", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
