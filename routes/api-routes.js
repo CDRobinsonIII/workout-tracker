49f78e03be579d75169fedd4a2676268835d2f7c
Charles Robinson <charlesdalerobinson@gmail.com>
2021-02-08 18:21:28 -0800

    Trying to figure out Heroku can't find module issue.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 // Enter routes here to perform CRUD operations with the MongoDB
 // When the user starts the application they will be able to either CREATE a new workout or continue (GET) last workout.
-
 const Workout = require("../models/Workout.js");
+
 const router = require("express").Router();
 
 // Create a new workout.
